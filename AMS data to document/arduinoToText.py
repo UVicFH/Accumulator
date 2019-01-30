a48e9725525174f052319e2b515958d4852bd3ba
cusitristan <cusitristan@gmail.com>
2019-01-29 19:17:04 -0800

    Added excel functionality
    Changed python script to export data into excel sheet instead of into a regular text file

--- AMS data to document/arduinoToText.py
+++ AMS data to document/arduinoToText.py
@@ -1,15 +1,32 @@
 #this library is what lets the script connect to the arduino serial terminal 
 import serial
-
+from xlwt import Workbook
 
 def main():
 
 	print('starting program now')
 	#telling script which port to use and baud#
-	ser = serial.Serial('COM4', 115200 )
-	
-	while True:
-		print (ser.readline())
+	ser = serial.Serial('COM6', 115200 )
+	line = []
+	#excel module stuff
+	wb = Workbook()
+	sheet = wb.add_sheet('accumulator sheet')
 
+	index_row = 1
+	while True:
+                #decoding byte type to string
+                line_not_decoded = ser.readline().decode('utf-8')
+                #line split up by ',' into a list
+                line = line_not_decoded.split(",")
+
+                index_col = 1
+                for word in line:
+                        
+                        sheet.write(index_row, index_col, word)
+                        index_col = index_col + 1
+                index_row = index_row + 1
+                #name of excel sheet
+                wb.save('accumulator_sheet.xls')
+                
 if __name__ == "__main__":
 	main()
